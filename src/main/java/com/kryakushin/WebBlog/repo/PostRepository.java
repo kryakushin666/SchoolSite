44e295af87dfa13733041da8272bbe48750d03cb
Artem <thebesthd123@gmail.com>
2022-01-18 17:29:42 +0400

    Added local MySQL database, error template, model and repository for post(/blog). Changed main icon in header.html.

---
+++ src/main/java/com/kryakushin/WebBlog/repo/PostRepository.java
@@ -0,0 +1,7 @@
+package com.kryakushin.WebBlog.repo;
+
+import com.kryakushin.WebBlog.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+}
